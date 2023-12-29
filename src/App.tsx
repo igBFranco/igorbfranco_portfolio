a9fc05db28ef6ce8c106003347a850cecdf09c1a
Igor Bueno Franco <88065589+igBFranco@users.noreply.github.com>
2023-12-29 15:10:25 -0300

    Revamping the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import Header from './components/Header';
 import styles from './App.module.scss';
 import Profile from './assets/profile.svg';
-import Instagram from './assets/instagram.png';
-import Github from './assets/github.png';
-import Linkedin from './assets/linkedin.png';
+import Instagram from './assets/instagram.svg';
+import Github from './assets/github.svg';
+import Linkedin from './assets/linkedin.svg';
 import Email from './assets/email.svg';
 import Location from './assets/location.svg';
 import Carousel from './components/Carousel';
 import Brasil from './assets/brasil.svg';
 import Luxembourg from './assets/luxembourg.svg';
+import LogoDark from './assets/LogoDark.svg'
 
 
 function App() {
@@ -20,13 +21,15 @@ function App() {
         <Header/>
         <div className={styles.primary}>
           <div className={styles.profile}>
-            <img src={Profile} alt="Igor Bueno Franco" />
-            <div className={styles.greeting}>
-              <h2>Olá,</h2>
+            {/* <img src={Profile} alt="Igor Bueno Franco" /> */}
+            {/* <div className={styles.greeting}>
+              <h3>Olá,</h3>
               <h3>eu sou o</h3>
               <h3>Igor Bueno Franco</h3>
-              <h4>Desenvolvedor Web e Mobile</h4>
-            </div>
+            </div> */}
+          </div>
+          <div className={styles.greetingText}>
+            <h2>Web and Mobile Developer, focused on providing high-quality digital experiences.</h2>
           </div>
         </div>
         <div className={styles.about} id="about">
@@ -43,13 +46,8 @@ function App() {
             </div>
             <div className={styles.aboutRight}>
               <p>
-                Apaixonado por Tecnologia desde sempre. Hoje com 22 anos, acadêmico de Engenharia de Software pela Universidade do Contestado - UnC.
+                Apaixonado por Tecnologia desde sempre. Hoje com 23 anos, Engenheiro de Software pela Universidade do Contestado - UnC.
               </p>
-              {/* <div className={styles.socials}>
-                <a href="https://github.com/igBFranco" target="blank"><img src={Github} alt="Github" /></a>
-                <a href="https://www.linkedin.com/in/igorbuenofranco/" target="blank"><img src={Linkedin} alt="Linkedin" /></a>
-                <a href="https://www.instagram.com/igorbfranco/" target="blank"><img src={Instagram} alt="Instagram" /></a>
-              </div> */}
             </div>
           </div>
         </div>
@@ -59,32 +57,33 @@ function App() {
         <div className={styles.contact} id="contact">
           <div className={styles.contactDetails}>
             <div className={styles.contactTitle}>
-              <h1>Contato</h1>
+              <h1>LET'S DEVELOP</h1>
+              <h2>Something amazing togheter</h2>
             </div>
-            <div className={styles.contactBox}>
-              <div className={styles.contactEmail}>
-                <img src={Email} alt="" />
-                <div className={styles.emailInfo}>
-                  <h2>Email</h2>
-                  <a href="mailto:contato@igorbuenofranco.com"><h4>contato@igorbuenofranco.com</h4></a>
-                </div>
-              </div>
-              <div className={styles.contactLocation}>
-                <img src={Location} alt="" />
-                <div className={styles.locationInfo}>
-                  <h2>Localização</h2>
-                  <h4>Itaiópolis - SC , Brasil</h4>
-                </div>
-              </div>
+            <div className={styles.contactButton}>
+              <a href="mailto:contato@igorbuenofranco.com">get in touch</a>
             </div>
           </div>
           {/* <Carousel images={images}/> */}
           <div className={styles.footer}>
-            <h1>Igor Bueno Franco</h1>
-            <div className={styles.footerBox}>
-                <a href="https://github.com/igBFranco" target="blank"><img src={Github} alt="Github" /></a>
-                <a href="https://www.linkedin.com/in/igorbuenofranco/" target="blank"><img src={Linkedin} alt="Linkedin" /></a>
-                <a href="https://www.instagram.com/igorbfranco/" target="blank"><img src={Instagram} alt="Instagram" /></a>
+            <div className={styles.logo}>
+              <img src={LogoDark} alt="Logo" />
+              <div className={styles.footerBox}>
+                  <a href="https://github.com/igBFranco" target="blank"><img src={Github} alt="Github" /></a>
+                  <a href="https://www.linkedin.com/in/igorbuenofranco/" target="blank"><img src={Linkedin} alt="Linkedin" /></a>
+                  <a href="https://www.instagram.com/igorbfranco/" target="blank"><img src={Instagram} alt="Instagram" /></a>
+              </div>
+            </div>
+            <div className={styles.copyright}>
+              <h4>© Igor Bueno Franco | 2023</h4>
+            </div>
+            <div className={styles.footerMenu}>
+              <ul>
+                  <li><a href="#home">Home</a></li>
+                  <li><a href="#about">About</a></li>
+                  {/* <li><a href="#projects">Projetos</a></li> */}
+                  <li><a href="#contact">Contact Me</a></li>
+              </ul>
             </div>
           </div>
         </div>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,27 @@
-
+import React, { useState } from 'react';
 import styles from './Header.module.scss';
+import LogoDark from '../../assets/LogoDark.svg';
+import MenuIcon from '../../assets/Menu.svg';
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuOpen = () => {
+    setMenuOpen(!menuOpen);
+  }
+
   return (
     <div className={styles.container}>
+        <img src={LogoDark} alt="Logo" />
+        {/* <div className={styles.menuIcon} onClick={handleMenuOpen}>
+          <img src={MenuIcon} alt="" />
+        </div> */}
         <div className={styles.navbar}>
             <ul>
                 <li><a href="#home">Home</a></li>
-                <li><a href="#about">Sobre</a></li>
+                <li><a href="#about">About</a></li>
                 {/* <li><a href="#projects">Projetos</a></li> */}
-                <li><a href="#contact">Contato</a></li>
+                <li><a href="#contact">Contact Me</a></li>
             </ul>
         </div>
     </div>
